a294645a1696902a3261b82290724d79987094f7
Daniel Lopez <dlopez@Daniels-MacBook-Pro.local>
2020-11-15 21:48:01 -0500

    Completed exercises and extra credit

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,110 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+function WelcomeForm({onSubmit, buttonText}) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+  const handleSubmit = e => {
+    e.preventDefault()
+    const formData = {
+      username: username,
+      password: password,
+    }
+    onSubmit(formData)
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username" />
+      <input
+        type="text"
+        id="username"
+        onChange={e => {
+          setUsername(e.target.value)
+        }}
+        value={username}
+      />
+      <label htmlFor="password" />
+      <input
+        type="text"
+        id="password"
+        onChange={e => {
+          setPassword(e.target.value)
+        }}
+        value={password}
+      />
+      <button type="submit">{buttonText}</button>
+    </form>
+  )
+}
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+function App() {
+  const [modal, setModal] = useState('none')
+
+  function handleSubmit(formData) {
+    console.log('Form Data: ', formData)
+  }
+
+  return (
+    <div>
+      <Logo />
+      <h1>Bookshelf</h1>
+      <button
+        onClick={() => {
+          setModal('login')
+        }}
+      >
+        login
+      </button>
+      <button
+        onClick={() => {
+          setModal('register')
+        }}
+      >
+        register
+      </button>
+
+      <Dialog
+        isOpen={modal === 'login'}
+        onDismiss={() => {
+          setModal('none')
+        }}
+        aria-labelledby={'Login Modal'}
+      >
+        <button
+          className="close-button"
+          onClick={() => {
+            setModal('none')
+          }}
+        >
+          <span aria-hidden>×</span>
+        </button>
+        <WelcomeForm onSubmit={handleSubmit} buttonText="LOGIN" />
+      </Dialog>
+
+      <Dialog
+        isOpen={modal === 'register'}
+        onDismiss={() => {
+          setModal('none')
+        }}
+        aria-labelledby={'Register Modal'}
+      >
+        <button
+          className="close-button"
+          onClick={() => {
+            setModal('none')
+          }}
+        >
+          <span aria-hidden>×</span>
+        </button>
+        <p>You've clicked on the Register modal</p>
+        <WelcomeForm onSubmit={handleSubmit} buttonText="REGISTER" />
+      </Dialog>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
